3952cd0df549cc14e876bc63199c2cbd1b279a75
Carl <Carl>
2016-12-08 22:39:37 +0100

    Still has bugs though new 3d-points seems good

--- main.m
+++ main.m
@@ -18,36 +18,46 @@ prevImage = imread(sprintf('data/%06d.png',0));
 
 %% Part 4 - Same, for all frames
 
+% Declaring tempstate for triangulation
+tempState = [];
+fakepose = [0,0,0]';
+
 figure(5);
-subplot(1, 3, 3);
+%subplot(1, 3, 3);
 scatter3(p_W_landmarks(1, :), p_W_landmarks(2, :), p_W_landmarks(3, :), 5);
 set(gcf, 'GraphicsSmoothing', 'on');
 view(0,0);
 axis equal;
 axis vis3d;
 axis([-15 10 -10 5 -1 40]);
-for i = 1:1
+for i = 1:20
+    disp(['Frame ' num2str(i) ' being processed!']);
     currImage = imread(sprintf('data/%06d.png',i));
     
-    [ currState, currPose ] = processFrame( prevState, prevImage, currImage, K );
+    [ currState, currPose, tempState,fakepose ] = processFrame_new(...
+        prevState, prevImage, currImage, K, tempState,fakepose );
     
     R_C_W = currPose(:,1:3);
     t_C_W = currPose(:,4);
 
     % Distinguish success from failure.
     if (numel(R_C_W) > 0)
-        subplot(1, 3, 3);
+        %subplot(1, 3, 3);
         plotCoordinateFrame(R_C_W', -R_C_W'*t_C_W, 2);
+        hold on
+        scatter3(currState(3, :), currState(4, :), currState(5, :), 5);
         view(0,0);
+        hold off
     else
         disp(['Frame ' num2str(i) ' failed to localize!']);
     end
     
-    subplot(1, 3, [1 2]);
-    imshow(currImage);
+%     subplot(1, 3, [1 2]);
+%     imshow(currImage);
     
     prevState = currState;
     prevImage = currImage;
 
     pause(0.01);
+    
 end
--- src/parameters.m
+++ src/parameters.m
@@ -10,16 +10,17 @@ global match_lambda;
 global use_p3p;
 global pose_dist_threshold;
 
+
 %% Pose Estimation
 use_p3p = true;
 
 %% Harris Corner Detector Parameters
 % Randomly chosen parameters that seem to work well
 harris_patch_size = 9;
-harris_kappa = 0.08; % Magic number in range (0.04 to 0.15)
-num_keypoints = 200;
+harris_kappa = 0.07; % Magic number in range (0.04 to 0.15)
+num_keypoints = 2000;
 nonmaximum_supression_radius = 8;
 descriptor_radius = 9;
-match_lambda = 4;
+match_lambda = 6;
 pose_dist_threshold = 0.10; % 10% used by Google Tango
 
